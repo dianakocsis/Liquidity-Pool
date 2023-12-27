e20ae39e54de1dd85390ce4ae34acd3e7c98f1c3
Diana Kocsis <dlk61@pitt.edu>
2023-12-27 15:13:59 -0500

    rearrange variables

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -9,34 +9,15 @@ contract Pool is ERC20 {
     SpaceCoin public immutable spaceCoin;
     uint256 public ethReserve;
     uint256 public spcReserve;
-    uint public constant MINIMUM_LIQUIDITY = 10**3;
-    uint112 private reserve0;           
-    uint112 private reserve1;
-
     bool public locked;
 
-    uint private unlocked = 1;
+    event Mint(address indexed sender, uint256  ethAmount, uint256  spcAmount);
+    event Burn(address indexed sender, uint256  ethAmount, uint256  spcAmount, address indexed to);
 
     error NoReentrancy();
     error InsufficientLiquidity();
     error InsufficientLiquidityBurned();
     error FailedToSendEther();
-
-    modifier lock() {
-        require(unlocked == 1, 'LOCKED');
-        unlocked = 0;
-        _;
-        unlocked = 1;
-    }
-
-    function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1) {
-        _reserve0 = reserve0;
-        _reserve1 = reserve1;
-    }
-
-    event Mint(address indexed sender, uint amount0, uint amount1);
-    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
-    event Sync(uint112 reserve0, uint112 reserve1);
 
     constructor(string memory _name, string memory _symbol, SpaceCoin _spaceCoin) ERC20(_name, _symbol) {
         spaceCoin = _spaceCoin;
@@ -51,12 +32,8 @@ contract Pool is ERC20 {
         locked = false;
     }
 
-
-    function _update(uint balance0, uint balance1) private {
-        require(balance0 <= 2**256-1 && balance1 <= 2**256-1, 'OVERFLOW');
-        reserve0 = uint112(balance0);
-        reserve1 = uint112(balance1);
-        emit Sync(reserve0, reserve1);
+    function getReserves() external view returns (uint256, uint256) {
+        return (ethReserve, spcReserve);
     }
 
     function mint(address _to) external nonReentrant returns (uint256 liquidity) {
@@ -113,34 +90,35 @@ contract Pool is ERC20 {
         spcReserve = spaceCoin.balanceOf(address(this));
     }
 
-    function swap(uint amountOut, address to, uint value) external lock {
-        require(amountOut > 0, 'INSUFFICIENT_OUTPUT_AMOUNT');
-        uint112 _reserve;
-        if (value > 0) {
-            (_reserve, ) = getReserves(); 
-        }
-        else {
-            (, _reserve ) = getReserves(); 
-        }
-        require(amountOut < _reserve, 'INSUFFICIENT_LIQUIDITY');
-        uint balance0;
-        uint balance1;
-        
-        require(to != address(spaceCoin), 'INVALID_TO');
-        if (value > 0) {
-            if (amountOut > 0) SpaceCoin(address(spaceCoin)).transfer(to, amountOut); 
-        }
-        else {
-            if (amountOut > 0) {
-                (bool sent,) = to.call{value: amountOut}("");
+    function swap(address _to) external nonReentrant returns (uint256 out) {
+        uint256 ethBalance = address(this).balance;
+        uint256 spcBalance = spaceCoin.balanceOf(address(this));
+        uint256 ethAmount = ethBalance - ethReserve;
+        uint256 spcAmount = spcBalance - spcReserve;
+        if (ethAmount > 0) {
+            uint256 amountEthWithFee = ethAmount * 99;
+            uint256 numerator = amountEthWithFee * spcReserve;
+            uint256 denominator = ethReserve * 100 + amountEthWithFee;
+            out = (numerator / denominator);
+            bool success = spaceCoin.transfer(_to, out);
+            if (!success) {
+                revert();
+            }
+        } else if (spcAmount > 0) {
+            uint256 amountSpcWithFee = spcAmount * 99;
+            uint256 numerator = amountSpcWithFee * ethReserve;
+            uint256 denominator = spcReserve * 100 + amountSpcWithFee;
+            out = (numerator / denominator);
+            (bool sent,) = _to.call{value: out}("");
+            if (!sent) {
+                revert FailedToSendEther();
             }
         }
-        balance0 = SpaceCoin(spaceCoin).balanceOf(address(this));
-        balance1 = address(this).balance;
-        
-        _update(balance0, balance1);
+
+        ethReserve = address(this).balance;
+        spcReserve = spaceCoin.balanceOf(address(this));
     }
-    
+
     receive() external payable {}
 
     function _min(uint256 a, uint256 b) internal pure returns (uint256) {
@@ -159,6 +137,4 @@ contract Pool is ERC20 {
             z = 1;
         }
     }
-
-
 }
