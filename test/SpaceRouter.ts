add280be6c2cb016e401f047f069b2cd79cb48c4
Diana Kocsis <dlk61@pitt.edu>
2023-12-27 14:41:47 -0500

    testing adding liquidity

--- test/SpaceRouter.ts
+++ test/SpaceRouter.ts
@@ -12,18 +12,39 @@ import {
   SpaceRouter,
 } from '../typechain-types';
 
-describe('SwapRouter', function () {
+describe('SpaceRouter', function () {
   let SpaceCoin: SpaceCoin__factory;
   let ICO: ICO__factory;
   let Pool: Pool__factory;
-  let SwapRouter: SpaceRouter__factory;
+  let SpaceRouter: SpaceRouter__factory;
   let spaceCoin: SpaceCoin;
   let pool: Pool;
   let ico: ICO;
-  let swapRouter: SpaceRouter;
+  let spaceRouter: SpaceRouter;
   let owner: SignerWithAddress,
     treasury: SignerWithAddress,
     addr1: SignerWithAddress;
+
+  const tokens = (count: string) => ethers.parseUnits(count, 18);
+
+  function sqrt(value: bigint) {
+    if (value < 0n) {
+      throw 'square root of negative numbers is not supported';
+    }
+
+    if (value < 2n) {
+      return value;
+    }
+
+    // Initial guess: Divide by 2n (bitwise right shift)
+    let x = value >> 1n;
+    let y = (x + value / x) >> 1n;
+    while (x > y) {
+      x = y;
+      y = (x + value / x) >> 1n;
+    }
+    return x;
+  }
 
   this.beforeEach(async function () {
     [owner, treasury, addr1] = await ethers.getSigners();
@@ -43,9 +64,84 @@ describe('SwapRouter', function () {
     pool = await Pool.deploy('Liquidity Pool', 'LP', spaceCoin);
     await pool.waitForDeployment();
 
-    SwapRouter = (await ethers.getContractFactory(
-      'SwapRouter'
+    SpaceRouter = (await ethers.getContractFactory(
+      'SpaceRouter'
     )) as SpaceRouter__factory;
-    swapRouter = (await SwapRouter.deploy(pool, spaceCoin)) as SpaceRouter;
+    spaceRouter = (await SpaceRouter.deploy(pool, spaceCoin)) as SpaceRouter;
+  });
+
+  it('Constructors', async function () {
+    expect(await spaceRouter.pool()).to.equal(await pool.getAddress());
+    expect(await spaceRouter.spaceCoin()).to.equal(
+      await spaceCoin.getAddress()
+    );
+    expect(await pool.name()).to.equal('Liquidity Pool');
+    expect(await pool.symbol()).to.equal('LP');
+    expect(await pool.spaceCoin()).to.equal(await spaceCoin.getAddress());
+  });
+
+  describe('Add Liquidity', function () {
+    it('Send liquidity to lp from treasury - first time', async function () {
+      await ico.connect(addr1).contribute({ value: tokens('1500') });
+      await ico.connect(owner).withdraw(tokens('1500'));
+      let ethReserve = await pool.ethReserve();
+      let spcReserve = await pool.spcReserve();
+      await spaceCoin
+        .connect(treasury)
+        .approve(spaceRouter.target, tokens('5'));
+      await spaceRouter
+        .connect(treasury)
+        .addLiquidity(treasury.getAddress(), tokens('5'), {
+          value: tokens('1'),
+        });
+      expect(await spaceCoin.balanceOf(pool.getAddress())).to.equal(
+        tokens('5')
+      );
+      expect(await ethers.provider.getBalance(pool.getAddress())).to.equal(
+        tokens('1')
+      );
+      expect(await pool.balanceOf(treasury)).to.equal(
+        sqrt(tokens('5') * tokens('1'))
+      );
+      expect(await pool.ethReserve()).to.equal(ethReserve + tokens('1'));
+      expect(await pool.spcReserve()).to.equal(spcReserve + tokens('5'));
+    });
+
+    it('emit Mint event', async function () {
+      await ico.connect(addr1).contribute({ value: tokens('1500') });
+      await ico.connect(owner).withdraw(tokens('1500'));
+      await spaceCoin
+        .connect(treasury)
+        .approve(spaceRouter.target, tokens('5'));
+      await expect(
+        spaceRouter
+          .connect(treasury)
+          .addLiquidity(treasury.getAddress(), tokens('5'), {
+            value: tokens('1'),
+          })
+      )
+        .to.emit(pool, 'Mint')
+        .withArgs(spaceRouter.target, tokens('1'), tokens('5'));
+    });
+
+    it('Second time adding liquidty', async function () {
+      await ico.connect(addr1).contribute({ value: tokens('1500') });
+      await ico.connect(owner).withdraw(tokens('1500'));
+      await spaceCoin
+        .connect(treasury)
+        .approve(spaceRouter.target, tokens('5'));
+      await spaceRouter
+        .connect(treasury)
+        .addLiquidity(treasury.getAddress(), tokens('5'), {
+          value: tokens('1'),
+        });
+      await ico.advancePhase(0);
+      await ico.advancePhase(1);
+      await ico.connect(addr1).redeem();
+      await spaceCoin.connect(addr1).approve(spaceRouter.target, tokens('5'));
+      await spaceRouter
+        .connect(addr1)
+        .addLiquidity(addr1.address, tokens('5'), { value: tokens('1') });
+    });
   });
 });
