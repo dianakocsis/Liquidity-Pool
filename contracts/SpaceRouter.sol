4d5e37288049cace9566b72b7434618eab312489
Diana Kocsis <dlk61@pitt.edu>
2023-12-27 15:14:27 -0500

    swapping

--- contracts/SpaceRouter.sol
+++ contracts/SpaceRouter.sol
@@ -10,6 +10,9 @@ contract SpaceRouter {
     SpaceCoin public immutable spaceCoin;
 
     error FailedToSendEther();
+    error InsufficientAmount();
+    error InsufficientLiquidity();
+    error InsufficientOutputAmount(uint256 out, uint256 min);
 
     constructor(Pool _lp, SpaceCoin _spaceCoin) {
         pool = _lp;
@@ -69,48 +72,57 @@ contract SpaceRouter {
         (ethAmount, spcAmount) = pool.burn(_to);
     }
 
-    function swapExactTokensForETH(
-        address token,
-        uint amountIn,
-        address to
-    ) external returns (uint amount) {
-        amount = _getOut(amountIn);
-        SpaceCoin(token).transferFrom(
-            msg.sender, address(pool), amountIn
-        );
-        Pool(pool).swap(amount, address(this), 0);
+    function swapSpcForEth(
+        uint256 _amountSpcIn,
+        uint256 _minEthOut,
+        address _to
+    )
+        external
+        returns (uint256 amountEthOut)
+    {
+        if (_amountSpcIn == 0) {
+            revert InsufficientAmount();
+        }
+
+        (uint256 ethReserve, uint256 spcReserve) = pool.getReserves();
+        if (ethReserve == 0 || spcReserve == 0) {
+            revert InsufficientLiquidity();
+        }
+
+        bool success = spaceCoin.transferFrom(msg.sender, address(pool), _amountSpcIn);
+        if (!success) {
+            revert();
+        }
+        amountEthOut = pool.swap(_to);
+
+        if (amountEthOut < _minEthOut) {
+            revert InsufficientOutputAmount(amountEthOut, _minEthOut);
+        }
     }
 
-    function swapExactETHForTokens(
-        address token,
-        address to
-    ) external payable returns (uint amount) {
-        amount = _getOut(msg.value);
+    function swapEthForSpc(
+        uint256 _minSpcOut,
+        address _to
+    )
+        external payable
+        returns (uint256 amountSpcOut)
+    {
+        if (msg.value == 0) {
+            revert InsufficientAmount();
+        }
+
+        (uint256 ethReserve, uint256 spcReserve) = pool.getReserves();
+        if (ethReserve == 0 || spcReserve == 0) {
+            revert InsufficientLiquidity();
+        }
+
         (bool sent,) = address(pool).call{value: msg.value}("");
         if (!sent) {
             revert FailedToSendEther();
         }
-        Pool(pool).swap(amount, address(this), msg.value);
-        SpaceCoin(token).transfer(to, amount);
-    }
-
-    function _getOut(uint amountIn) internal returns (uint amountOut) {
-        require(amountIn > 0, 'INSUFFICIENT_INPUT_AMOUNT');
-        uint reserveOut;
-        uint reserveIn;
-        if (msg.value > 0) {
-            (reserveOut, reserveIn ) = Pool(pool).getReserves();
+        amountSpcOut = pool.swap(_to);
+        if (amountSpcOut < _minSpcOut) {
+            revert InsufficientOutputAmount(amountSpcOut, _minSpcOut);
         }
-        else {
-            (reserveIn, reserveOut ) = Pool(pool).getReserves();
-        }
-        require(reserveIn > 0 && reserveOut > 0, 'INSUFFICIENT_LIQUIDITY');
-        uint amountInWithFee = amountIn * 999; // 1% trading fee
-        uint numerator = amountInWithFee * reserveOut;
-        uint denominator = reserveIn * 1000 + amountInWithFee;
-        amountOut = numerator / denominator;
     }
-
-    receive() external payable {}
-
 }
